c71f7643e4b3868b06e6dd989537b0ff50787dfb
venkateshsingh-byte <venkatesh.singh@plugincomm.com>
2025-10-15 17:55:49 +0530

    Update branch.js

--- model/branch.js
+++ model/branch.js
@@ -5,7 +5,7 @@ const branchSchema = new mongoose.Schema({
   branch_img: { type: String },
   branch_map: { type: String }, // ✅ Google Maps URL stored here
   branch_all_add: { type: String },
-  slug_url: { type: String },
+  slug_url: { type: String, index: true, unique: true, required: true },
   meta_title: { type: String },
   meta_desc: { type: String },
   dateCreated: { type: Date, default: Date.now }
