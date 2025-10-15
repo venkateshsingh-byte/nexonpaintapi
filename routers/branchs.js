4c0e0c70e276c4a079797e87207e3212e8a6f057
venkateshsingh-byte <venkatesh.singh@plugincomm.com>
2025-10-15 17:55:03 +0530

    Update branchs.js

--- routers/branchs.js
+++ routers/branchs.js
@@ -4,6 +4,7 @@ const BranchController = require('../controller/branchs');
 const uploadBranchImg = require('../helper/uploadImages')
 
 router.get('/',BranchController.getBranch)
+router.get('/slug/:slug',BranchController.getBranchBySlug)
 router.get('/:id',BranchController.getBranchById)
 router.post('/add',uploadBranchImg,BranchController.addBranch)
 router.put('/edit/:id',uploadBranchImg,BranchController.editBranch);
